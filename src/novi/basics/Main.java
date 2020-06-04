84e9dda6d304043b412ec2ff23d5c80485629fd8
Sunshine Summerville <sunshine.summerville@gmail.com>
2020-06-04 18:16:22 +0200

    Hello Sunshine

---
+++ src/novi/basics/Main.java
@@ -0,0 +1,12 @@
+package novi.basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Sunshine");
+        int getal = 0;
+        //type          maam              //waarde -> object
+        LoginController loginController = new loginController();
+    }
+}
